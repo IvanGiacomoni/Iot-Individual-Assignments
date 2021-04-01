6cbf1a972ecc7c30516085053c8f82d987a71fca
IvanGiacomoni <62717293+IvanGiacomoni@users.noreply.github.com>
2021-04-01 16:02:20 +0200

    added body-parser

--- dashboard/startup/routes.js
+++ dashboard/startup/routes.js
@@ -2,7 +2,13 @@ const getData = require("../routes/getData");
 const views = require("../routes/views");
 const controlActuators = require("../routes/controlActuators");
 
+var bodyParser = require('body-parser')
+
+
 module.exports = function (app) {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }))
+  
   app.use("/", getData);
   app.use("/", views);
   app.use("/", controlActuators);
