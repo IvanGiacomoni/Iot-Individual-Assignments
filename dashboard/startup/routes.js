25ea7bc9045d0ee2caa20361b144c90bf1d4c6a3
IvanGiacomoni <62717293+IvanGiacomoni@users.noreply.github.com>
2021-04-01 10:54:27 +0200

    added controlActuators

--- dashboard/startup/routes.js
+++ dashboard/startup/routes.js
@@ -1,7 +1,9 @@
 const getData = require("../routes/getData");
 const views = require("../routes/views");
+const controlActuators = require("../routes/controlActuators");
 
 module.exports = function (app) {
   app.use("/", getData);
   app.use("/", views);
+  app.use("/", controlActuators);
 };
