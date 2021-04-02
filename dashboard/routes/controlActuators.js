31fd8a1c95bc9a190ebc8f517a77631f98d00403
IvanGiacomoni <62717293+IvanGiacomoni@users.noreply.github.com>
2021-04-02 12:20:14 +0200

    modified switch route/added actuator manag. route

--- dashboard/routes/controlActuators.js
+++ dashboard/routes/controlActuators.js
@@ -30,7 +30,43 @@ router.post("/switchMode", async (req, res) => {
 		.on('connect', function() {
 			console.log('connect');
 			device.subscribe(myTopic);
-			device.publish(myTopic, JSON.stringify({mode: mode}));
+			device.publish(myTopic, mode+'\0');
+		});
+		
+		device
+		.on('message', function(topic, payload) {
+			console.log('message', topic, payload.toString());
+		})
+	
+	res.status(200).send("done!");
+});
+
+router.post("/manageActuators", async (req, res) => {
+	
+	const device_id = req.body.device_id;
+	const actuator = req.body.actuator;
+	const dataType = req.body.dataType;
+	const operation = req.body.operation;
+	
+	const myTopic = "manageActuators/device/"+device_id;
+	
+	console.log("topic: "+myTopic+"\n");
+	
+	
+		var device = awsIot.device({
+			keyPath: path.resolve(__dirname, process.env.PRIVATE_KEY),
+			certPath: path.resolve(__dirname, process.env.CERT),
+			caPath: path.resolve(__dirname, process.env.ROOT_CA),
+			clientId: process.env.CLIENT,
+			host: process.env.HOST
+		});	
+	
+		// Device is an instance returned by mqtt.Client()
+		device
+		.on('connect', function() {
+			console.log('connect');
+			device.subscribe(myTopic);
+			device.publish(myTopic, actuator+";"+dataType+";"+operation+'\0');
 		});
 		
 		device
