64aaa9137df512e2cf3a7b442f92e60a747710a0
IvanGiacomoni <62717293+IvanGiacomoni@users.noreply.github.com>
2021-05-10 18:10:35 +0200

    Delete dashboard directory

--- dashboard/routes/views.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const path = require("path");
-
-router.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "../views", "dashboard.html"));
-});
-
-module.exports = router;
